1c461fed4415fd8c3d84d1c52699c8559fea9c19
atakan yüce <atakanyc@gmail.com>
2021-04-17 14:57:43 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsInto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> sayılar = new List<int> { 1, 2, 3, 4 };
+            foreach (var item in sayılar)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("/////////");
+
+            MyList<int> sayılar2 = new MyList<int> {  };
+
+            Console.WriteLine(sayılar2.Length);
+
+            sayılar2.Add(6);
+
+            Console.WriteLine(sayılar2.Length);
+
+            sayılar2.Add(8);
+            sayılar2.Add(15);
+            sayılar2.Add(30);
+
+            foreach (var item in sayılar2.Items)
+            {
+                Console.WriteLine(item);
+
+            }
+
+
+
+
+        }
+    }
+}
